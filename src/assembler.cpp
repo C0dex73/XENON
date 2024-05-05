35d617eecd57d61a2d0fb40ae0d78894a986a0dd
C0dex73 <c0d3x73@gmail.com>
2024-05-05 16:54:44 +0200

    added path support in header file

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
+#include <cstring>
+#include "paths.h"
 using namespace std;
-
-/*struct paths {
-    private:
-    char *xnn;
-    char *schem;
-    char *format(char *adr){
-
-    }
-    public:
-    paths(char *xnn, char *schem=(char*)(' ')){
-         
-        if(schem == ""){ schem = xnn; }
-    }
-    char *getXnn(){ return this->xnn; }
-    char *getSchem() { return this->schem; }
-};*/
 
 int main(int argc, char *argv[]) {
     if(argc < 2 || argc > 3) {
-        fprintf(stderr, "No enough arguments, use the assembler like this : './assember.exe -\"program.xnn\" -\"schem.schematic\"' to compile the code of the xnn file into the schematic file.");
+        fprintf(stderr, "Not enough arguments, use the assembler like this : './assember.exe -\"program.xnn\" -\"schem.schematic\"' to compile the code of the xnn file into the schematic file.");
         return -1;
     }
-    printf("okay");
-
+    
+    Paths paths(argc, argv);
+    if(paths.getErr() != ""){
+        fprintf(stderr, paths.getErr());
+        return -1;
+    }
+    cout << paths.getXnn() << " --> " << paths.getSchem();
     return 0;
 }
