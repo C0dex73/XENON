e3433d9b48453f093b0421b56abcc90e9076faa9
C0dex73 <c0d3x73@gmail.com>
2024-05-12 18:41:55 +0200

    modified error code if not correct arguments

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -6,10 +6,9 @@
 int main(int argc, char *_argv[]) {
     std::vector<std::string> argv(&_argv[1], &_argv[argc]);
     if(argc < 2 || argc > 3) {
-        fprintf(stderr, "Not enough arguments, use the assembler like this : './assember.exe -\"program.xnn\" -\"schem.schematic\"' to compile the code of the xnn file into the schematic file.");
-        return -1;
+        fprintf(stderr, "Not enough arguments, use the assembler like this : './assember.exe -\"program.xnn\" -\"assemblyfile.xas\"' to compile the code of the xnn file into the assembly file.");
+        return 11;
     }
     PathHandler paths = PathHandler(argv);
-    std::cout << paths.getSchem() << "\r\n" << paths.getXnn() << "\r\n";
     return 0;
 }
