41cce28dc23c8a52758f8bf894d48b15f2b892d2
C0dex73 <c0d3x73@gmail.com>
2024-05-12 18:44:22 +0200

    use the utils::ProcessResult in funcs

--- src/string_processing.cpp
+++ src/string_processing.cpp
@@ -1,10 +1,25 @@
 #include <string>
 #include "string_processing.h"
+#include "process_result.h"
 
-bool utils::startsWith(std::string str, std::string prefix){
-    return str.size() >= prefix.size() && str.compare(0, prefix.size(), prefix) == 0;
+utils::ProcessResult<bool> utils::startsWith(std::string str, std::string prefix){
+    utils::ProcessResult<bool> res;
+    res.res = str.size() >= prefix.size() && str.compare(0, prefix.size(), prefix) == 0;
+    res.SEC = 0;
+    return res;
 }
 
-bool utils::endsWith(std::string str, std::string suffix){
-    return str.size() >= suffix.size() && str.compare(str.size()-suffix.size(), suffix.size(), suffix) == 0;
+utils::ProcessResult<bool> utils::endsWith(std::string str, std::string suffix){
+    utils::ProcessResult<bool> res;
+    res.res = str.size() >= suffix.size() && str.compare(str.size()-suffix.size(), suffix.size(), suffix) == 0;
+    res.SEC = 0;
+    return res;
+}
+
+utils::ProcessResult<std::string> utils::safeSuffixRemove(std::string str, std::string suffix){
+    std::string _str = str;
+    utils::ProcessResult<std::string> res;
+    res.res = utils::endsWith(str, suffix).res ? str.erase(str.length()-suffix.length()) : str;
+    res.SEC = res.res == _str ? 87 : 0;
+    return res;
 }
--- src/string_processing.h
+++ src/string_processing.h
@@ -2,10 +2,12 @@
 #define XENON_STRING_PROCESSING
 
 #include <string>
+#include "process_result.h"
 
 namespace utils {
-    bool startsWith(std::string str, std::string prefix);
-    bool endsWith(std::string str, std::string suffix);
+    ProcessResult<bool> startsWith(std::string str, std::string prefix);
+    ProcessResult<bool> endsWith(std::string str, std::string suffix);
+    ProcessResult<std::string> safeSuffixRemove(std::string str, std::string suffix);
 }
 
 #endif
